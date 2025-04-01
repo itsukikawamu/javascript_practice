8e3a922c2286698e6056d0ad2582a150229c4f5e
itsukikawamu <itsuki0204@gmail.com>
2025-04-01 22:43:07 +0900

    add fs writefile

--- src/useModule.js
+++ src/useModule.js
@@ -1,7 +1,23 @@
 const { Person, judge } = require('./module');
-
 const p1 = new Person("Mike", 19);
 p1.introduce();
 judge(p1);
 
-process.arg
+console.log(process.argv);
+
+
+
+function writeContent(err){
+    if (err) {
+        console.error('failed:', err);
+        return;
+    }
+    else {
+        console.log("succeeded.");
+    }
+};
+
+const fs = require('fs');
+const pass = '../text/profile.txt';
+const content = "i am hungry.";
+f = fs.writeFile(pass, content, writeContent);
