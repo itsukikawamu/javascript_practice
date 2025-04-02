9a2eba2fdf69c57c70f6ce508b043f7bd9cef054
itsukikawamu <itsuki0204@gmail.com>
2025-04-02 16:01:47 +0900

    add cbh

--- src/handleFile.js
+++ src/handleFile.js
@@ -14,22 +14,22 @@ const fs = require('fs');
 fs.writeFile(pass, hungry_message, writeResult);
 
 let content = "Initial message.";
-fs.writeFile("../text/test1.txt", content, ()=>{
-    fs.readFile("../text/test1.txt", 'utf8', (err, data)=>{
+fs.writeFile("../text/cbh1.txt", content, ()=>{
+    fs.readFile("../text/cbh1.txt", 'utf8', (err, data)=>{
         if (err){
             console.log("failed.", err);
             return;
         }
         content = `succeeded.\ndata: ${data}`;
-        fs.writeFile("../text/test2.txt", content, ()=>{
-            fs.readFile("../text/test2.txt", 'utf8', (err, data)=>{
+        fs.writeFile("../text/cbh2.txt", content, ()=>{
+            fs.readFile("../text/cbh2.txt", 'utf8', (err, data)=>{
                 if (err){
                     console.log("failed.", err);
                     return;
                 }
                 content = `succeeded.\ndata: ${data}`;
-                fs.writeFile("../text/test3.txt", content, ()=>{
-                    fs.readFile("../text/test3.txt", 'utf8', (err, data)=>{
+                fs.writeFile("../text/cbh3.txt", content, ()=>{
+                    fs.readFile("../text/cbh3.txt", 'utf8', (err, data)=>{
                         if (err){
                             console.log("failed.", err);
                             return;
