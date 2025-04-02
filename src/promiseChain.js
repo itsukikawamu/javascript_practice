87883d8eaa5afe630dbee2eb12c637744d6375bd
itsukikawamu <itsuki0204@gmail.com>
2025-04-02 20:37:50 +0900

    add promiseChain

--- src/promiseChain.js
+++ src/promiseChain.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 const promiseWriteFile=(path, content)=>{
     return new Promise((resolve, reject)=>{
-        fs.writeFile(path, content, (err, data)=>{
+        fs.writeFile(path, content, (err)=>{
             if (err) reject(err);
             else resolve();
         })
@@ -10,7 +10,7 @@ const promiseWriteFile=(path, content)=>{
 };
 const promiseReadFile=(path)=>{
     return new Promise((resolve, reject)=>{
-        fs.readFile(path, (err, data)=>{
+        fs.readFile(path, 'utf8', (err, data)=>{
             if (err) reject(err);
             else resolve(data);
         })
@@ -31,4 +31,12 @@ promiseWriteFile("../text/promise1.txt", content)
 })
 .then(()=> promiseReadFile("../text/promise2.txt")
 //{}を書かない場合returnを省略するとpromiseReadFileの返り値がそのまま変える仕様
-);
+)
+.then((data)=>{
+    content = `succeeded.\ndata: ${data}`;
+    return promiseWriteFile("../text/promise3.txt", content);
+})
+.then(()=> promiseReadFile("../text/promise3.txt"))
+.catch((err)=>{
+    console.log("failed:", err);
+});
